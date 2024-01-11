413ab40f74aebbe30f3b8175fb1d704351fe554b
portal2312 <portal2312@gmail.com>
2024-01-11 23:50:46 +0900

    6.1. AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        cardColor: const Color(0xFFF4EDDB),
-        colorScheme: const ColorScheme(
-          background: Color(0xFFE7626C),
-          brightness: Brightness.light,
-          primary: Color(0xFFFFFFFF),
-          onPrimary: Color(0xFFFFFFFF),
-          secondary: Color(0xFFFFFFFF),
-          onSecondary: Color(0xFFFFFFFF),
-          error: Color(0xFFFFFFFF),
-          onError: Color(0xFFFFFFFF),
-          onBackground: Color(0xFFFFFFFF),
-          surface: Color(0xFFFFFFFF),
-          onSurface: Color(0xFFFFFFFF),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-      ),
-      home: const Scaffold(
-        body: HomeScreen(),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,147 +1,23 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late Timer timer;
-  static int initSeconds = 5;
-  static int initPomodoros = 0;
-  bool isRunning = false;
-  int totalPomodoros = initPomodoros;
-  int totalSeconds = initSeconds;
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  void onReplayPressed() {
-    setState(() {
-      isRunning = false;
-      totalPomodoros = initPomodoros;
-      totalSeconds = initSeconds;
-    });
-    timer.cancel();
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(const Duration(seconds: 1), onTick);
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        isRunning = false;
-        totalPomodoros += 1;
-        totalSeconds = initSeconds;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds -= 1;
-      });
-    }
-  }
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-    return duration.toString().split('.').first.substring(2, 7);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 89,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        shadowColor: Colors.grey.shade200,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
           ),
-          Flexible(
-            flex: 3,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    color: Theme.of(context).cardColor,
-                    icon: Icon(isRunning
-                        ? Icons.pause_circle_outlined
-                        : Icons.play_circle_outline),
-                    iconSize: 98,
-                    onPressed: isRunning ? onPausePressed : onStartPressed,
-                  ),
-                  IconButton(
-                      color: Theme.of(context).cardColor,
-                      icon: const Icon(Icons.replay_outlined),
-                      iconSize: 46,
-                      onPressed: onReplayPressed),
-                ],
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Theme.of(context).cardColor,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pomodoros',
-                          style: TextStyle(
-                            color:
-                                Theme.of(context).textTheme.displayLarge?.color,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          '$totalPomodoros',
-                          style: TextStyle(
-                            color:
-                                Theme.of(context).textTheme.displayLarge?.color,
-                            fontSize: 58,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
